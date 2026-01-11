0b5312be7cd2405f7e99edaef0d4d101b181c430
mukhammadrizo2009 <muhammadrizomirzaev671@gmail.com>
2026-01-11 16:41:40 +0500

    Html

--- apps/skills/models.py
+++ apps/skills/models.py
@@ -2,7 +2,15 @@ from django.db import models
 
 class Skill(models.Model):
     name = models.CharField(max_length=50)
-    level = models.PositiveIntegerField(help_text="0-100")
-    
+    logo = models.ImageField(upload_to="skills/")
+    level = models.CharField(
+        max_length=20,
+        choices=[
+            ("Beginner", "Beginner"),
+            ("Intermediate", "Intermediate"),
+            ("Advanced", "Advanced"),
+        ]
+    )
+
     def __str__(self):
         return self.name
