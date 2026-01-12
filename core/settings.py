512770648660b6fd05319e7f26df870be704c2f0
mukhammadrizo2009 <muhammadrizomirzaev671@gmail.com>
2026-01-12 19:51:02 +0500

    templates

--- core/settings.py
+++ core/settings.py
@@ -28,11 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = [
-    'mirzayeoff.pythonanywhere.com',
-    'www.mirzayeoff.pythonanywhere.com',
-]
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
